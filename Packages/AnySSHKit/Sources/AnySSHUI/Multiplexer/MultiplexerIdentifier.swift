3978d9646e1a4d63901abf2fff97294a9894957f
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 09:59:38 -0500

    refactor: add pane list failure and attaching identifiers

--- Packages/AnySSHKit/Sources/AnySSHUI/Multiplexer/MultiplexerIdentifier.swift
+++ Packages/AnySSHKit/Sources/AnySSHUI/Multiplexer/MultiplexerIdentifier.swift
@@ -2,8 +2,13 @@ public enum MultiplexerIdentifier {
     public static let open = "multiplexer.open"
     public static let paneList = "mux.panes"
     public static let paneListClose = "mux.panes.close"
+    public static let paneListFailure = "mux.panes.failure"
 
     public static func pane(_ id: String) -> String {
         "mux.pane.\(id)"
+    }
+
+    public static func attaching(_ id: String) -> String {
+        "mux.attaching.\(id)"
     }
 }
