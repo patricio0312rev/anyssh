41f63a259fd05745d81210c0c28434c38b0631d1
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 01:55:43 -0500

    feat: add settings identifiers to registry

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Settings/UIIdentifierSettings.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension UIIdentifier {
+    public enum Settings {
+        public static let open = "settings.open"
+        public static let screen = "settings.screen"
+        public static let close = "settings.close"
+        public static let gestures = "settings.gestures"
+        public static let snippets = "settings.snippets"
+        public static let about = "settings.about"
+        public static let privacy = "settings.privacy"
+    }
+}
