8773fb571e15c011b71f232e2d119b45d5ddd380
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 07:49:16 -0500

    refactor: add workspace motion animations

--- Packages/AnySSHKit/Sources/AnySSHUI/Theme/ThemeMotion.swift
+++ Packages/AnySSHKit/Sources/AnySSHUI/Theme/ThemeMotion.swift
@@ -4,6 +4,8 @@ extension Theme {
     public enum Motion {
         public static let standard: Animation = .default
         public static let spring: Animation = .spring(response: 0.32, dampingFraction: 0.86)
+        public static let overlay: Animation = .smooth(duration: 0.28)
+        public static let sessionSwitch: Animation = .smooth(duration: 0.3)
         public static let copyFeedbackDwell: Duration = .seconds(1.6)
     }
 }
