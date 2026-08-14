1b8f5c18811d14ce1b389d26b840b8c6f5709fcd
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 01:55:11 -0500

    feat: add trust identifiers to registry

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Auth/HostKey/UIIdentifierTrust.swift
@@ -0,0 +1,11 @@
+extension UIIdentifier {
+    public enum Trust {
+        public static let accept = "trust.accept"
+        public static let reject = "trust.reject"
+        public static let cancel = "trust.cancel"
+        public static let forget = "trust.forget"
+        public static let offeredFingerprint = "trust.fingerprint.offered"
+        public static let storedFingerprint = "trust.fingerprint.stored"
+        public static let dismiss = "trust.dismiss"
+    }
+}
