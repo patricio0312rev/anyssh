2ec7dcc7ad61904393062ad7b771c6d04850a654
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 07:48:39 -0500

    feat: identify workspace browser UI elements

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Sessions/Browser/UIIdentifierWorkspaceBrowser.swift
@@ -0,0 +1,6 @@
+extension UIIdentifier.Workspace {
+    public static let open = "workspace.open"
+    public static let sheet = "workspace.sheet"
+    public static let modeToggle = "workspace.mode.toggle"
+    public static let resolving = "workspace.resolving"
+}
