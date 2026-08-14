c6f2bc84de1dded6a49653c466fc98084461b345
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 02:57:12 -0500

    feat: add accessibility identifiers to accessory

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Terminal/Accessory/UIIdentifierAccessory.swift
@@ -0,0 +1,12 @@
+extension UIIdentifier.Terminal {
+    public enum Accessory {
+        public static let bar = "terminal.accessory.bar"
+        public static let preview = "terminal.accessory.preview"
+        public static let overflow = "terminal.accessory.overflow"
+        public static let menu = "terminal.accessory.menu"
+        public static let reorder = "terminal.accessory.reorder"
+        public static let add = "terminal.accessory.add"
+        public static let addPrefix = "terminal.accessory.addPrefix"
+        public static let reset = "terminal.accessory.reset"
+    }
+}
