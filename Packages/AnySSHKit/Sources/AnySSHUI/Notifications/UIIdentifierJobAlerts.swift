bffdc1aaed5827d139d3b16b38e0a06299c12ecf
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 09:58:19 -0500

    feat: port and rename UI identifier for job alerts, drop dead code

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Notifications/UIIdentifierJobAlerts.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+extension UIIdentifier {
+    public enum JobAlerts {
+        public static let open = "jobAlerts.open"
+        public static let close = "jobAlerts.close"
+        public static let banner = "jobAlerts.banner"
+        public static let systemRequests = "jobAlerts.systemRequests"
+        public static let settings = "jobAlerts.settings"
+        public static let quietToggle = "jobAlerts.quietToggle"
+        public static let snippet = "jobAlerts.snippet"
+        public static let copySnippet = "jobAlerts.copySnippet"
+    }
+}
