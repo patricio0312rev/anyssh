709cb0cbf64e4863dc598c17ba3edd2c60301c9c
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 01:54:54 -0500

    feat: add key import identifiers to registry

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Remotes/KeyImport/UIIdentifierKeyImport.swift
@@ -0,0 +1,14 @@
+extension UIIdentifier {
+    public enum KeyImport {
+        public static let screen = "remote.keyImport"
+        public static let headline = "remote.keyImport.headline"
+        public static let paste = "remote.keyImport.paste"
+        public static let file = "remote.keyImport.file"
+        public static let fingerprint = "remote.keyImport.fingerprint"
+        public static let algorithm = "remote.keyImport.algorithm"
+        public static let passphrase = "remote.keyImport.passphrase"
+        public static let save = "remote.keyImport.save"
+        public static let discard = "remote.keyImport.discard"
+        public static let saved = "remote.keyImport.saved"
+    }
+}
