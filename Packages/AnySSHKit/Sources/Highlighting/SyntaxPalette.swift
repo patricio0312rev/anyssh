05af54e0ddc80997cb7ad9e48ba329e331e7b192
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 13:11:07 -0500

    fix: clean up design tokens in syntax palette

--- Packages/AnySSHKit/Sources/Highlighting/SyntaxPalette.swift
+++ Packages/AnySSHKit/Sources/Highlighting/SyntaxPalette.swift
@@ -9,19 +9,6 @@ public enum SyntaxRole: String, CaseIterable, Sendable {
     case property
     case comment
     case plain
-
-    public var sRGBHex: String {
-        switch self {
-        case .keyword: "#ff6188"
-        case .string: "#ffd866"
-        case .function: "#a9dc76"
-        case .type: "#78dce8"
-        case .constant: "#ab9df2"
-        case .property: "#fc9867"
-        case .comment: "#727072"
-        case .plain: "#fcfcfa"
-        }
-    }
 }
 
 public enum SyntaxPalette {
