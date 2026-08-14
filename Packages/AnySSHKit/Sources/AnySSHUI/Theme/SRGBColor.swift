2f7b8abcd17ff6d4dfaa8b56cbc291f440adc915
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 02:57:15 -0500

    feat: add sRGB color extension to UIColor

--- Packages/AnySSHKit/Sources/AnySSHUI/Theme/SRGBColor.swift
+++ Packages/AnySSHKit/Sources/AnySSHUI/Theme/SRGBColor.swift
@@ -11,3 +11,18 @@ extension Color {
         )
     }
 }
+
+#if canImport(UIKit)
+import UIKit
+
+extension UIColor {
+    static func srgb(_ hex: UInt32) -> UIColor {
+        UIColor(
+            red: CGFloat((hex >> 16) & 0xff) / 255,
+            green: CGFloat((hex >> 8) & 0xff) / 255,
+            blue: CGFloat(hex & 0xff) / 255,
+            alpha: 1
+        )
+    }
+}
+#endif
