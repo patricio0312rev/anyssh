4f33430fa201e9c601c3fa535b4d9e0ec76b8862
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 03:37:15 -0500

    feat: add folder icon mappings (part 3)

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Files/Icons/FileIconFolderTable3.swift
@@ -0,0 +1,14 @@
+nonisolated enum FileIconFolderTable3: Sendable {
+    static let table: [String: String] = [
+        "srcs": "folder-src",
+        "submodules": "folder-git",
+        "test": "folder-test",
+        "testfiles": "folder-test",
+        "testing": "folder-test",
+        "tests": "folder-test",
+        "texture": "folder-images",
+        "textures": "folder-images",
+        "wiki": "folder-docs",
+        "xtask": "folder-scripts",
+    ]
+}
