54a70876488d15160b5abfd1e91ee4434de2404a
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 08:26:27 -0500

    chore: add settings scenario composition

---
+++ AnySSH/Composition/SettingsScenario.swift
@@ -0,0 +1,9 @@
+enum SettingsScenario: String, Equatable, CaseIterable {
+    case sheet = "settings"
+    case gestures = "settings.gestures"
+    case snippets = "settings.snippets"
+    case snippetsEmpty = "settings.snippets.empty"
+    case privacy = "settings.privacy"
+    case about = "settings.about"
+    case notice = "settings.about.notice"
+}
