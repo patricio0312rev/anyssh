bc9665aa2f3a79f7f5353bfcae401563e49d19b0
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 02:33:51 -0500

    refactor: make RemoteRoute public for direct form launching

--- Packages/AnySSHKit/Sources/AnySSHUI/Remotes/RemoteRoute.swift
+++ Packages/AnySSHKit/Sources/AnySSHUI/Remotes/RemoteRoute.swift
@@ -1,11 +1,11 @@
 import AnySSHCore
 
-enum RemoteRoute: Identifiable, Hashable {
+public enum RemoteRoute: Identifiable, Hashable {
     case add
     case edit(Remote)
     case importKey
 
-    var id: String {
+    public var id: String {
         switch self {
         case .add: "add"
         case .edit(let remote): "edit.\(remote.id.rawValue)"
