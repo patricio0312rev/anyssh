a600c60be51001552ea156cf4b8bbf33681b4e89
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 09:07:47 -0500

    test: name the new-file changes scenario

--- AnySSH/Composition/GitScenario.swift
+++ AnySSH/Composition/GitScenario.swift
@@ -1,6 +1,7 @@
 enum GitScenario: String, Equatable, CaseIterable {
     case changes = "git.changes"
     case changesExpanded = "git.changes.expanded"
+    case changesNewFile = "git.changes.newFile"
     case changesClean = "git.changes.clean"
     case history = "git.history"
     case historyEmpty = "git.history.empty"
