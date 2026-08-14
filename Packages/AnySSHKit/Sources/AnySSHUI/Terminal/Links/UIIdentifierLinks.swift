b95109616f61c57d3521e2e08dd3b394f883f95e
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 13:11:31 -0500

    fix: clean up design tokens in link identifiers

--- Packages/AnySSHKit/Sources/AnySSHUI/Terminal/Links/UIIdentifierLinks.swift
+++ Packages/AnySSHKit/Sources/AnySSHUI/Terminal/Links/UIIdentifierLinks.swift
@@ -3,5 +3,6 @@ extension UIIdentifier.Terminal {
         public static let open = "terminal.link.open"
         public static let copyLink = "terminal.link.copyLink"
         public static let copy = "terminal.link.copy"
+        public static let copied = "terminal.link.copied"
     }
 }
