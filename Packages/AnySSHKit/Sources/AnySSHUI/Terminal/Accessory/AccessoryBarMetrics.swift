110a87c224e93012e6c27cadae825b38071f2d38
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 02:57:12 -0500

    feat: port accessory bar metrics

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Terminal/Accessory/AccessoryBarMetrics.swift
@@ -0,0 +1,8 @@
+import CoreGraphics
+
+public enum AccessoryBarMetrics {
+    public static let height: CGFloat = 44
+    public static let keyHeight = height - Theme.Space.step1 * 2
+    public static let keyFontSize: CGFloat = 15
+    public static let hairline: CGFloat = 1
+}
