c770a169fa21e33707d4341553ca7ab06c4b9b3d
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 03:37:38 -0500

    feat: add diff line metrics for layout

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Git/DiffRenderer/DiffMetrics.swift
@@ -0,0 +1,9 @@
+import CoreGraphics
+
+enum DiffMetrics {
+    static let lineNumberColumn: CGFloat = 34
+    static let markerColumn: CGFloat = 16
+    static let rowLeading: CGFloat = 1
+    static let gutterWidth = lineNumberColumn * 2 + markerColumn
+    static let inlineHeight: CGFloat = 420
+}
