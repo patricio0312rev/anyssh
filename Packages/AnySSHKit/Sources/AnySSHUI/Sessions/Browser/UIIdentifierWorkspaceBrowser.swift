e262dbb4372a7a98e74a118590815f156ec340f6
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 14:00:42 -0500

    refactor: delete the unreferenced workspace open identifier

--- Packages/AnySSHKit/Sources/AnySSHUI/Sessions/Browser/UIIdentifierWorkspaceBrowser.swift
+++ Packages/AnySSHKit/Sources/AnySSHUI/Sessions/Browser/UIIdentifierWorkspaceBrowser.swift
@@ -1,5 +1,4 @@
 extension UIIdentifier.Workspace {
-    public static let open = "workspace.open"
     public static let sheet = "workspace.sheet"
     public static let modeToggle = "workspace.mode.toggle"
     public static let resolving = "workspace.resolving"
