5e1c9393288faf8ecbb214894592d9b643ee3b4f
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 09:07:43 -0500

    refactor: drop the fixed gutter width

--- Packages/AnySSHKit/Sources/AnySSHUI/Git/DiffRenderer/DiffMetrics.swift
+++ Packages/AnySSHKit/Sources/AnySSHUI/Git/DiffRenderer/DiffMetrics.swift
@@ -4,6 +4,5 @@ enum DiffMetrics {
     static let lineNumberColumn: CGFloat = 34
     static let markerColumn: CGFloat = 16
     static let rowLeading: CGFloat = 1
-    static let gutterWidth = lineNumberColumn * 2 + markerColumn
     static let inlineHeight: CGFloat = 420
 }
