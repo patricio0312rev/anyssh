701ef4d8a78570ba9a592c02daf5bafd473d6d64
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 02:57:13 -0500

    feat: add accessibility to gestures

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Terminal/Interaction/UIIdentifierGestures.swift
@@ -0,0 +1,9 @@
+extension UIIdentifier.Terminal {
+    public enum Gestures {
+        public static let selectionEnd = "terminal.gestures.selectionEnd"
+        public static let scrollOffset = "terminal.gestures.scrollOffset"
+        public static let route = "terminal.gestures.route"
+        public static let sessionSwitcher = "terminal.session.switcher"
+        public static let touchMode = "terminal.gestures.touchMode"
+    }
+}
