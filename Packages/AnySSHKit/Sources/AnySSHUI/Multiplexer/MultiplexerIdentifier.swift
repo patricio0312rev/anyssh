7593e9441a897deca6fe02dfe8c205daea1a835e
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 06:54:05 -0500

    feat: register pane list accessibility identifiers

--- Packages/AnySSHKit/Sources/AnySSHUI/Multiplexer/MultiplexerIdentifier.swift
+++ Packages/AnySSHKit/Sources/AnySSHUI/Multiplexer/MultiplexerIdentifier.swift
@@ -1,3 +1,9 @@
 public enum MultiplexerIdentifier {
     public static let open = "multiplexer.open"
+    public static let paneList = "mux.panes"
+    public static let paneListClose = "mux.panes.close"
+
+    public static func pane(_ id: String) -> String {
+        "mux.pane.\(id)"
+    }
 }
