376536c67918ed371890e02752b905acaf2cf6db
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 01:54:06 -0500

    feat: add keychain migration probe identifier

--- Packages/AnySSHKit/Sources/AnySSHUI/Components/UIIdentifier.swift
+++ Packages/AnySSHKit/Sources/AnySSHUI/Components/UIIdentifier.swift
@@ -1,4 +1,8 @@
 public enum UIIdentifier {
+    public enum Launch {
+        public static let keychainMigrationRuns = "launch.keychainMigrationRuns"
+    }
+
     public enum Workspace {
         public static let wrapToggle = "workspace.files.preview.wrap"
     }
