0fd5083408ee3b1cf0a541f17c57a10f270e015c
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 02:57:14 -0500

    feat: add accessibility to links

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Terminal/Links/UIIdentifierLinks.swift
@@ -0,0 +1,7 @@
+extension UIIdentifier.Terminal {
+    public enum Links {
+        public static let open = "terminal.link.open"
+        public static let copyLink = "terminal.link.copyLink"
+        public static let copy = "terminal.link.copy"
+    }
+}
