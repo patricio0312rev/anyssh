a4572e81d387b3c086626a8e373e10d2b56102e7
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 01:12:41 -0500

    feat: reduce chrome typography to four Dynamic Type roles

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Theme/ThemeText.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+extension Theme {
+    public enum Text {
+        public static let screenTitle: Font = .largeTitle.bold()
+        public static let sectionHeader: Font = .subheadline.weight(.semibold)
+        public static let body: Font = .body
+        public static let caption: Font = .caption
+    }
+}
