b0e5eef9721df942f957e048134d8b9c73a217b7
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 00:33:59 -0500

    feat: define terminal gesture interaction policy

---
+++ Packages/AnySSHKit/Sources/TerminalEmulator/Gestures/GesturePolicy.swift
@@ -0,0 +1,44 @@
+public enum TerminalGestureRoute: String, CaseIterable, Hashable, Sendable {
+    case scrollback
+    case remoteApp
+    case remoteKeys
+    case selection
+}
+
+public struct TerminalGestureMode: Hashable, Sendable {
+    public let alternateScreen: Bool
+    public let mouseReporting: Bool
+    public let touchMode: Bool
+    public let shiftHeld: Bool
+
+    public init(
+        alternateScreen: Bool,
+        mouseReporting: Bool,
+        touchMode: Bool,
+        shiftHeld: Bool
+    ) {
+        self.alternateScreen = alternateScreen
+        self.mouseReporting = mouseReporting
+        self.touchMode = touchMode
+        self.shiftHeld = shiftHeld
+    }
+}
+
+public enum TerminalGesturePolicy {
+    public static func route(for mode: TerminalGestureMode) -> TerminalGestureRoute {
+        if mode.shiftHeld {
+            return .selection
+        }
+        if mode.mouseReporting && !mode.touchMode {
+            return .remoteApp
+        }
+        if mode.alternateScreen && !mode.touchMode {
+            return .remoteKeys
+        }
+        return .scrollback
+    }
+
+    public static func dragShouldBegin(route: TerminalGestureRoute, selectionActive: Bool) -> Bool {
+        true
+    }
+}
