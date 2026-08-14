e480f14b0cb0048775851338752d20aaff6eae4a
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-14 01:12:41 -0500

    feat: fix margins, padding and radii to the 4pt grid

---
+++ Packages/AnySSHKit/Sources/AnySSHUI/Theme/ThemeSpace.swift
@@ -0,0 +1,22 @@
+import CoreGraphics
+
+extension Theme {
+    public enum Space {
+        public static let step1: CGFloat = 4
+        public static let step2: CGFloat = 8
+        public static let step3: CGFloat = 12
+        public static let step4: CGFloat = 16
+        public static let step5: CGFloat = 24
+        public static let step6: CGFloat = 32
+
+        public static let screenMargin: CGFloat = 16
+        public static let cardPadding: CGFloat = 12
+        public static let rowGap: CGFloat = 8
+
+        public static let cardRadius: CGFloat = 12
+        public static let controlRadius: CGFloat = 8
+
+        public static let iconTile: CGFloat = 40
+        public static let iconGlyph: CGFloat = 20
+    }
+}
