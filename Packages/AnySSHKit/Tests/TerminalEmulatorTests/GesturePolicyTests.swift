e5c1ea69edd2c856be5b59f96446eabbdd9bb948
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2026-08-17 21:47:08 -0500

    test: cover stationary remoteApp clicks

--- Packages/AnySSHKit/Tests/TerminalEmulatorTests/GesturePolicyTests.swift
+++ Packages/AnySSHKit/Tests/TerminalEmulatorTests/GesturePolicyTests.swift
@@ -16,6 +16,19 @@ struct GesturePolicyTests {
     }
 
     @Test
+    func aStationaryRemoteAppDragIsAClick() {
+        #expect(
+            TerminalGesturePolicy.shouldReportClick(route: .remoteApp, didTravel: false)
+        )
+        #expect(
+            !TerminalGesturePolicy.shouldReportClick(route: .remoteApp, didTravel: true)
+        )
+        for route in [TerminalGestureRoute.scrollback, .remoteKeys, .selection] {
+            #expect(!TerminalGesturePolicy.shouldReportClick(route: route, didTravel: false))
+        }
+    }
+
+    @Test
     func ourPanBeginsOnEveryRoute() {
         for route in [TerminalGestureRoute.scrollback, .remoteApp, .remoteKeys, .selection] {
             #expect(TerminalGesturePolicy.dragShouldBegin(route: route, selectionActive: false))
